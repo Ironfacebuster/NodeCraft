c1e65d04122bc0bba16f1f4b927b12453bd8e90b
thesonny <41223890+the-sonny@users.noreply.github.com>
2020-10-29 12:53:46 -0400

    Create poop.js

---
+++ server data/commands/poop.js
@@ -0,0 +1,10 @@
+module.exports.data = {
+    name: "/poop",
+    description: "It poops.",
+    op: false,
+    requireCache: true
+}
+
+module.exports.execute = (data) => {
+    //Under construction!
+}
